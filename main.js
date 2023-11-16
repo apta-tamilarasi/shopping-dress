d9b7753e7f8e409c03e38662fb709681b8995b45
apta-tamilarasi <athinarayanantamilarasi@gamil.com>
2023-11-16 09:26:52 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,14 @@
+let a=prompt("Enter the amount")
+
+if(a>500 && a<=1000){
+    document.write("sarees available")
+}
+else if(a>1000 && a<=1500){
+    document.write("Kid frag available")
+}
+else if(a>1500 && a<=2000){
+    document.write("Leganka Available")
+} 
+else{
+    document.write("Alll dress are available")
+}
